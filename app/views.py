064b455066050ac5040b6485ad46914e0640f5f2
aswandhkolangarodi <aswandhk2000@gmail.com>
2022-06-04 12:18:33 +0530

    v2

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from app.models import Coffe
 import razorpay
 # Create your views here.
 
@@ -6,8 +7,12 @@ import razorpay
 def payment(request):
     if request.method == "POST":
         name = request.POST.get('name')
-        amount = request.POST.get('amount')
+        amount = int(request.POST.get('amount')) * 100
         client = razorpay.Client(auth = ("rzp_test_bKtMj90QOs6Af2", "vNLvdBnrIGSHG2C4BiWoDGvd"))
+        payment = client.order.create({'amount':amount, 'currency':'INR', 'payment_capture':'1'})
+        print(payment)
+        coffe = Coffe(name=name, amound=amount, paiment_id=payment['id'])
+        return render(request,'app/index.html', {'payment':payment})
     return render(request,'app/index.html')
 
 
